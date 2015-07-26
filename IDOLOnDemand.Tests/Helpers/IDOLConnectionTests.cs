865c874705c4bfdc1cf1789ebf6cc469c5454171
Peter Smith <peter@reservedwords.net>
2015-07-26 21:51:25 +1200

    Added stub test method

--- IDOLOnDemand.Tests/Helpers/IDOLConnectionTests.cs
+++ IDOLOnDemand.Tests/Helpers/IDOLConnectionTests.cs
@@ -1,7 +1,17 @@
-﻿namespace IDOLOnDemand.Tests.Helpers
+﻿using IDOLOnDemand.Helpers;
+using NUnit.Framework;
+
+namespace IDOLOnDemand.Tests.Helpers
 {
+    [TestFixture]
     public class IDOLConnectionTests
     {
-         
+        [Test]
+        public void Given_an_IDOLConnection_When_making_a_request_Then_the_request_has_an_API_Key_parameter()
+        {
+            IDOLConnection.Connect(new {Foo = 10, Bar = "baz"}, "this-endpoint");
+            
+            Assert.That(true, Is.True);
+        }
     }
 }
